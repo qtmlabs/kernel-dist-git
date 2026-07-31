60dd492f8281260b5b27d278674b02b222530128
Justin M. Forbes <jforbes@fedoraproject.org>
2026-07-31 10:04:15 -0600

    kernel-7.2.0-0.rc5.260731g8ba098e6b6ff.45
    * Fri Jul 31 2026 Fedora Kernel Team <kernel-team@fedoraproject.org> [7.2.0-0.rc5.8ba098e6b6ff.45]
    - automotive: enable HUGETLBFS to workaround build error (Scott Weaver)
    Resolves:
    
    Signed-off-by: Justin M. Forbes <jforbes@fedoraproject.org>

--- filtermods.py
+++ filtermods.py
@@ -568,7 +568,7 @@ def resolve_remaining(pkg_list: KModPackageList, kmod_list: KModList):
     for pkg_obj in pkg_list:
         if pkg_obj.default:
             if default_pkg:
-                log.error('Already have default pkg: %s / %s', default_pkg.name, pkg_obj.name)
+                log.warning('Already have default pkg: %s / %s', default_pkg.name, pkg_obj.name)
             else:
                 default_pkg = pkg_obj
 
